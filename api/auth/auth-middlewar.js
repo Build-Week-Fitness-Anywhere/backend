82da4cf2adb65a10282afac8a1152641ed097a02
Joe Witzke <joewitzke@joes-mbp.lan>
2021-06-22 08:27:15 -0600

    POST login working

--- api/auth/auth-middlewar.js
+++ api/auth/auth-middlewar.js
@@ -33,8 +33,21 @@ const checkRegistration = (req, res, next) => {
   }
 };
 
+const validateRoleName = (req, res, next) => {
+  if (req.body.role === 'instructor' || req.body.role.trim() === 'instructor') {
+    next();
+  } else if (req.body.role === 'client' || req.body.role.trim() === 'client') {
+    next();
+  } else if (req.body.role.trim().length > 32) {
+    next({ status: 422, message: 'Role name can not be longer than 32 chars' });
+  } else {
+    next({ status: 422, message: 'Must be a instructor or client' });
+  }
+};
+
 module.exports = {
   restricted,
   checkIfString,
   checkRegistration,
+  validateRoleName,
 };
--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -2,16 +2,43 @@ const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const tokenBuilder = require('./token-builder');
 const User = require('../users/users-model');
-const { checkIfString, checkRegistration } = require('./auth-middlewar');
+const {
+  checkIfString,
+  checkRegistration,
+  validateRoleName,
+} = require('./auth-middlewar');
 
-router.post('/register', checkIfString, checkRegistration, (req, res, next) => {
-  let user = req.body;
-  const rounds = process.env.BCRYPT_ROUNDS || 8;
-  const hash = bcrypt.hashSync(user.password, rounds);
-  user.password = hash;
-  User.add(user)
-    .then((user) => {
-      res.status(201).json(user);
+router.post(
+  '/register',
+  checkIfString,
+  checkRegistration,
+  validateRoleName,
+  (req, res, next) => {
+    let user = req.body;
+    const rounds = process.env.BCRYPT_ROUNDS || 8;
+    const hash = bcrypt.hashSync(user.password, rounds);
+    user.password = hash;
+    User.add(user)
+      .then((user) => {
+        res.status(201).json(user);
+      })
+      .catch(next);
+  }
+);
+
+router.post('/login', (req, res, next) => {
+  const { username, password } = req.body;
+  User.findBy({ username })
+    .then(([user]) => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = tokenBuilder(user);
+        res.status(200).json({
+          message: `Welcome, ${user.username}`,
+          token: token,
+        });
+      } else {
+        res.status(401).json({ message: 'invalid credentials' });
+      }
     })
     .catch(next);
 });
